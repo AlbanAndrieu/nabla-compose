81405aab4a578bb60d5107c6491efd5943c2f1b0
AlbanAndrieu <alban.andrieu@free.fr>
2026-09-08 21:37:21 +0200

    test(k8s): require actionable Kubara ingress failure

--- tests/test_kubernetes_fastapi_smoke_contract.py
+++ tests/test_kubernetes_fastapi_smoke_contract.py
@@ -74,6 +74,7 @@ class KubernetesFastApiSmokeContractTests(unittest.TestCase):
         required = (
             "--preflight",
             'kubectl get ingressclass "${INGRESS_CLASS}"',
+            "complete the reviewed Kubara v0.14.0 ingress bootstrap",
             "spec.controller",
             "kubectl get ingress --all-namespaces -o json",
             "Ingress host ${HOST} is already claimed by",
